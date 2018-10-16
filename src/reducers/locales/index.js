7721c395dc561a3ec3ed2b80530f2b8008ce9a92
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-10-16 19:18:50 +0200

    Create getAllLocales selector

--- src/reducers/locales/index.js
+++ src/reducers/locales/index.js
@@ -1,3 +1,4 @@
 export { NAMESPACE } from './locales.const'
 export { fetchAllLocales } from './locales.actions'
+export { getAllLocales } from './locales.selectors'
 export { default as locales } from './locales.reducer'
