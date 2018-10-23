a9bbbb1f32d8b147ada07035e21a1ab318317157
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-10-23 07:38:00 +0200

    Get locale from url to set default locale and replace the path

--- src/reducers/locales/locales.actions.js
+++ src/reducers/locales/locales.actions.js
@@ -1,10 +1,24 @@
+import { isEmpty } from 'lodash-es'
+
 import {
   REQUEST_IN_PROGRESS,
   REQUEST_STATUS_SUCCESS,
-  REQUEST_STATUS_FAIL
+  REQUEST_STATUS_FAIL,
+  SET_ACTIVE_LOCALE
 } from './locales.const'
 
 import { getLocales } from '@themoviedb/the-movie-db-fetching'
+
+const getLocaleFromUrl = () => {
+  let splitPath = window.location.pathname.split('/')
+  
+  if (isEmpty(splitPath[ 1 ])) {
+    window.history.replaceState(null, 'en', '/en')
+    splitPath = window.location.pathname.split('/')
+  }
+
+  return splitPath[ 1 ]
+}
 
 const requestInProgress = () => {
   return {
@@ -31,11 +45,32 @@ export const fetchAllLocales = () => {
     dispatch(requestInProgress())
 
     try {
-      const { data } = await getLocales()
-      dispatch(requestSuccessful(data))
+      const response = await getLocales()
+      const localeInURL = getLocaleFromUrl()
+      const defaultLocale = response.find(locale => locale.default)
+
+      let activeLocale = {}
+
+      //set active locale to locale found in URL
+      activeLocale = response.find(locale => locale.code === localeInURL)
+
+      if (isEmpty(activeLocale)) {
+        //if locale not found in URL or invalid
+        //set activeLocale to English
+        activeLocale = defaultLocale
+      }
+
+      dispatch(requestSuccessful({ activeLocale, response }))
     } catch (e) {
       dispatch(requestFail(e))
       throw e
     }
+  }
+}
+
+export const setActiveLocale = payload => {
+  return {
+    type: SET_ACTIVE_LOCALE,
+    payload
   }
 }
