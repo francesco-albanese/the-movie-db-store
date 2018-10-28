aec03abe8de8be9251aa0135cae64c8e8d1f8f85
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-10-28 16:17:14 +0100

    exposing setActiveLocale

--- src/reducers/locales/index.js
+++ src/reducers/locales/index.js
@@ -1,4 +1,4 @@
 export { NAMESPACE } from './locales.const'
-export { fetchAllLocales } from './locales.actions'
+export { fetchAllLocales, setActiveLocale } from './locales.actions'
 export { getActiveLocale, getAllLocales } from './locales.selectors'
 export { default as locales } from './locales.reducer'
